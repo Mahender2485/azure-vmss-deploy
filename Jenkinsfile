feaf6e6c4a6ca25bd56475aea475ff2c012c0860
Mahender2485 <games.kondreddy@gmail.com>
2025-08-05 07:05:26 -0500

    Initial Commit

---
+++ Jenkinsfile
@@ -0,0 +1,60 @@
+pipeline {
+  agent any
+
+  environment {
+    ARM_CLIENT_ID       = ''
+    ARM_CLIENT_SECRET   = ''
+    ARM_SUBSCRIPTION_ID = ''
+    ARM_TENANT_ID       = ''
+  }
+
+  stages {
+    stage('Checkout Code') {
+      steps {
+        git url: 'https://github.com/Mahender2485/azure-vmss-deploy.git'
+      }
+    }
+
+    stage('Load Azure Credentials') {
+      steps {
+        withCredentials([string(credentialsId: 'AZURE_CREDENTIALS_JSON', variable: 'AZURE_CREDENTIALS')]) {
+          writeFile file: 'azure_creds.json', text: AZURE_CREDENTIALS
+          script {
+            def creds = readJSON file: 'azure_creds.json'
+            env.ARM_CLIENT_ID       = creds.clientId
+            env.ARM_CLIENT_SECRET   = creds.clientSecret
+            env.ARM_SUBSCRIPTION_ID = creds.subscriptionId
+            env.ARM_TENANT_ID       = creds.tenantId
+          }
+        }
+      }
+    }
+
+    stage('Terraform Init') {
+      steps {
+        sh 'terraform init'
+      }
+    }
+
+    stage('Terraform Plan') {
+      steps {
+        sh 'terraform plan -out=tfplan'
+      }
+    }
+
+    stage('Terraform Apply') {
+      steps {
+        sh 'terraform apply -auto-approve tfplan'
+      }
+    }
+  }
+
+  post {
+    success {
+      slackSend channel: '#jenkin-pipeline', message: "✅ *Build succeeded*: ${env.JOB_NAME} #${env.BUILD_NUMBER}"
+    }
+    failure {
+      slackSend channel: '#jenkin-pipeline', message: "❌ *Build failed*: ${env.JOB_NAME} #${env.BUILD_NUMBER}"
+    }
+  }
+}
---
+++ main.tf
@@ -0,0 +1,87 @@
+provider "azurerm" {
+  features = {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "vmss-rg"
+  location = "canadacentral"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vmss-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "vmss-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_public_ip" "public_ip" {
+  name                = "vmss-public-ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_lb" "lb" {
+  name                = "vmss-lb"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "Standard"
+  frontend_ip_configuration {
+    name                 = "LoadBalancerFrontEnd"
+    public_ip_address_id = azurerm_public_ip.public_ip.id
+  }
+}
+
+resource "azurerm_lb_backend_address_pool" "bepool" {
+  loadbalancer_id = azurerm_lb.lb.id
+  name            = "backend-pool"
+}
+
+resource "azurerm_linux_virtual_machine_scale_set" "vmss" {
+  name                = "vmss-demo"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "Standard_B1s"
+  instances           = 2
+  admin_username      = "azureuser"
+  admin_password      = "P@ssword1234!" # Use strong password
+  disable_password_authentication = false
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
+
+  network_interface {
+    name    = "nic"
+    primary = true
+
+    ip_configuration {
+      name                                   = "internal"
+      subnet_id                              = azurerm_subnet.subnet.id
+      load_balancer_backend_address_pool_ids = [azurerm_lb_backend_address_pool.bepool.id]
+      primary                                = true
+    }
+  }
+
+  upgrade_mode = "Manual"
+}
+
+output "public_ip_address" {
+  value = azurerm_public_ip.public_ip.ip_address
+}
